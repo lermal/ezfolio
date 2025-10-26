b77f93dbd9a1e1bb30be71459c2aaf56b5ba85cb
Mr. Samuel <82917953+lermal@users.noreply.github.com>
2025-10-26 14:22:09 +0300

    Update sitemap.blade.php

--- resources/views/frontend/sitemap.blade.php
+++ resources/views/frontend/sitemap.blade.php
@@ -1,12 +1,10 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-@foreach($urls as $url)
     <url>
-        <loc>{{ $url['loc'] }}</loc>
-        <lastmod>{{ $url['lastmod'] }}</lastmod>
-        <changefreq>{{ $url['changefreq'] }}</changefreq>
-        <priority>{{ $url['priority'] }}</priority>
+        <loc>{{ url('/') }}</loc>
+        <lastmod>{{ date('Y-m-d') }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
     </url>
-@endforeach
 </urlset>
