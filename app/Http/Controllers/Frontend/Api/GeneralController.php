c49f86e7476ffc0eed18f37cb811a003f7295b98
Mr. Samuel <82917953+lermal@users.noreply.github.com>
2025-10-26 09:55:34 +0300

    Update GeneralController.php

--- app/Http/Controllers/Frontend/Api/GeneralController.php
+++ app/Http/Controllers/Frontend/Api/GeneralController.php
@@ -8,6 +8,7 @@ use App\Services\Contracts\MessageInterface;
 use CoreConstants;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class GeneralController extends Controller
 {
