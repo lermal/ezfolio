da1bcfcd4eb2c79a00142c080f4094fb6e434bb9
Mr. Samuel <82917953+lermal@users.noreply.github.com>
2025-10-27 12:24:50 +0300

    Update custom.blade.php

--- resources/views/frontend/theme/custom.blade.php
+++ resources/views/frontend/theme/custom.blade.php
@@ -109,15 +109,11 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top shadow-sm">
         <div class="container">
-            <a class="navbar-brand fw-bold" href="#home">
-                <span class="text-primary">{{substr($about->name, 0, 1)}}</span>{{substr($about->name, 1)}}
-            </a>
-            
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
             
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="#home">{{__('custom.nav.home')}}</a>
